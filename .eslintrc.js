cb27db2e2543206c566a5d76e76a4374c774bfd9
Alan Albertengo <alan.albertengo@gmail.com>
2022-03-29 12:46:53 -0300

    Add geolocation

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,18 +8,39 @@ module.exports = {
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:prettier/recommended',
-    'airbnb'
+    'airbnb',
+    'airbnb-typescript'
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaFeatures: {
-      jsx: true
-    },
-    ecmaVersion: 'latest',
-    sourceType: 'module'
+    ecmaVersion: 2020,
+    project: './tsconfig.json'
   },
   plugins: ['react', '@typescript-eslint'],
   rules: {
-    'react/react-in-jsx-scope': 'off'
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [1, { extensions: ['.tsx', '.ts'] }],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never'
+      }
+    ],
+    'object-curly-newline': 'off',
+    'comma-dangle': 'off',
+    '@typescript-eslint/comma-dangle': [
+      'error',
+      {
+        arrays: 'never',
+        objects: 'never',
+        imports: 'never',
+        exports: 'never',
+        functions: 'never'
+      }
+    ]
   }
 };
--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,13 +1,33 @@
 import React from 'react';
-import { Flex, Button, Text } from '@chakra-ui/react';
+import { Flex, Button, Text, Box } from '@chakra-ui/react';
+import useGeolocation from 'react-navigator-geolocation';
 
 function HomePage() {
+  const { isAvailable, isEnabled, coords, suppressRequest } = useGeolocation({
+    suppressOnMount: false,
+    positionOptions: { enableHighAccuracy: false, timeout: 5000, maximumAge: 0 }
+  });
+
+  if (!isAvailable) {
+    // Your browser doesn't support Geolocation API
+    return null;
+  }
+
+  if (!isEnabled) {
+    return (
+      <Flex className="HomePage" direction="column">
+        <Button colorScheme="blue" width="200px" onClick={() => suppressRequest(false)}>
+          <Text fontSize="lg">Get current location</Text>
+        </Button>
+      </Flex>
+    );
+  }
+
   return (
-    <Flex className="HomePage" direction="column">
-      <Button colorScheme="blue" width="200px">
-        <Text fontSize="lg">Get current location</Text>
-      </Button>
-    </Flex>
+    <Box>
+      <Text fontSize="2xl">Coordinates granted</Text>
+      <Text fontSize="xl">{`${coords?.latitude}, ${coords?.longitude}`}</Text>
+    </Box>
   );
 }
 
